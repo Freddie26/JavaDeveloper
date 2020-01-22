807aa0fa61bfb6c71e061733e0c0eab6a6b6cde5
Freddie26 <igor.anackii@gmail.com>
2020-01-23 00:30:17 +0300

    переписан алгоритм получения размера папки

--- module 9/1/src/main/java/Main.java
+++ module 9/1/src/main/java/Main.java
@@ -2,6 +2,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.File;
+import java.nio.file.*;
 import java.util.Scanner;
 
 public class Main {
@@ -10,42 +11,26 @@ public class Main {
     private static Logger logger = LogManager.getLogger(Main.class.getName());
 
     public static void main(String[] args) {
-        logger.info("Введите путь папки для получения информации о файлах в ней:");
+        //noinspection InfiniteLoopStatement
+        for (;;) {
+            logger.info("Введите путь папки для получения информации о файлах в ней:");
+            try {
+                Path folderPath = Paths.get(scanner.nextLine());
 
-        try {
-            //noinspection InfiniteLoopStatement
-            for (;;) {
-                String folderPath = scanner.nextLine();
+                if (Files.isDirectory(folderPath)) {
+                    long folderLength = Files.walk(folderPath, Integer.MAX_VALUE, FileVisitOption.FOLLOW_LINKS)
+                            .filter(p -> !Files.isDirectory(p))
+                            .map(Path::toFile)
+                            .mapToLong(File::length)
+                            .sum();
 
-                File initFolder = new File(folderPath);
-                if (initFolder.isDirectory()) {
-                    scanFolder(initFolder);
-                }
-                else {
+                    logger.info("Размер папки [{}] байт", folderLength);
+                } else {
                     logger.warn("Переданный путь [{}] не является директорией!", folderPath);
                 }
-            }
-        } catch (Exception ex) {
-            logger.error(ex);
-        }
-    }
-
-    private static void scanFolder(File folder) {
-        File[] files = folder.listFiles();
-
-        if (files == null) {
-            logger.info("Папка {} не содержит файлов", folder.getPath());
-            return;
-        }
-
-        for(File file: files) {
-            if (file.isFile()) {
-                logger.info("Файл [{}], расположенный в [{}] имеет размер - [{}] кб",
-                        file.getName(), folder.getAbsolutePath(), file.length() / 1024);
-            } else {
-                scanFolder(file);
+            } catch (Exception ex) {
+                logger.error(ex);
             }
         }
     }
-
 }
